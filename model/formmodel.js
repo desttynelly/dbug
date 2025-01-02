8be09acd088199f5e054faf971f47b18fd97e298
desttynelly <obhiosad@gmail.com>
2025-01-02 13:13:48 +0100

    first commit

--- model/formmodel.js
+++ model/formmodel.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+// Define the user schema
+const userSchema = new mongoose.Schema(
+  {
     troy: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      trim: true, // Removes whitespace from the start and end
     },
-}, {
-    timestamps: true
-});
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
 
+// Export the model
 module.exports = mongoose.model('Troy', userSchema);
